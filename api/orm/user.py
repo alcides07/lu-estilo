98b5925868339e1246617d3200b698faf26ba519
alcides07 <alcidesdantasdj@gmail.com>
2025-05-22 12:00:33 -0300

    feat: implementa hash de senha. (#11)

--- api/orm/user.py
+++ api/orm/user.py
@@ -1,10 +1,18 @@
 from api.dependencies.get_session_db import SessionDep
 from api.models.user import User
 from api.schemas.user import UserCreate
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"])
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 
 def create_user(user: UserCreate, session: SessionDep) -> User:
-    db_user = User(**user.model_dump())
+    user.password = get_password_hash(user.password)
+    db_user = User(**user.model_dump(exclude=set(["passwordConfirmation"])))
 
     with session.begin():
         session.add(db_user)
