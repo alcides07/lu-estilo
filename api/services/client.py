0f3a0eee07d9f17e439e64fdccb410186843c04d
alcides07 <alcidesdantasdj@gmail.com>
2025-05-23 01:59:04 -0300

    refactor: especifica tipagem de servicos de validacao de client.

--- api/services/client.py
+++ api/services/client.py
@@ -4,13 +4,17 @@ from sqlalchemy.orm import Session
 from fastapi import HTTPException
 
 
-def check_cpf_exists(session: Session, cpf: str) -> None:
+def check_cpf_exists(session: Session, cpf: str) -> bool:
     """Verifica se CPF já está cadastrado"""
     if exists(session, Client, cpf=cpf):
         raise HTTPException(400, "Já existe um cliente com esse CPF")
 
+    return False
 
-def check_user_client_exists(session: Session, user_id: int) -> None:
+
+def check_user_client_exists(session: Session, user_id: int) -> bool:
     """Verifica se já existe um cliente vinculado ao usuário fornecido"""
     if exists(session, Client, user_id=user_id):
         raise HTTPException(400, "Já existe um cliente associado a esse usuário")
+
+    return False
