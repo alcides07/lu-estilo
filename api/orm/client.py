41d0804e29aef2550f1ed5dc96d8817e63bcba28
alcides07 <alcidesdantasdj@gmail.com>
2025-05-22 21:22:49 -0300

    refactor: renomeia funcao de cliente existente. (#17)

--- api/orm/client.py
+++ api/orm/client.py
@@ -5,14 +5,14 @@ from api.schemas.client import ClientCreate
 from api.orm.utils.get_object_or_404 import get_object_or_404
 from fastapi import HTTPException
 from sqlalchemy.exc import SQLAlchemyError
-from api.services.client import check_cpf_exists, check_user_exists
+from api.services.client import check_cpf_exists, check_user_client_exists
 
 
 def create_client(client: ClientCreate, session: SessionDep) -> Client:
     db_client = Client(**client.model_dump())
     get_object_or_404(session, User, client.user_id, detail="Usuário não encontrado")
     check_cpf_exists(session, client.cpf)
-    check_user_exists(session, client.user_id)
+    check_user_client_exists(session, client.user_id)
 
     try:
         session.add(db_client)
