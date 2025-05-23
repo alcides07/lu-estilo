1d35d534fc504f51f5fe2da3e6e7009958c65bf2
alcides07 <alcidesdantasdj@gmail.com>
2025-05-22 21:21:10 -0300

    feat: implementa rotas de administrator. (#17)

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -1,4 +1,5 @@
 from .user import router as user_router
 from .client import router as client_router
+from .administrator import router as administrator_router
 
-all_routers = [user_router, client_router]
+all_routers = [user_router, client_router, administrator_router]
