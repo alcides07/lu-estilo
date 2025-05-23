c2374438a86d992d458b78e82284986c9a2018af
alcides07 <alcidesdantasdj@gmail.com>
2025-05-23 01:52:37 -0300

    feat: implementa schemas de autenticacao. (#20)

---
+++ api/schemas/auth.py
@@ -0,0 +1,34 @@
+from datetime import timedelta
+from pydantic import BaseModel, Field
+from enum import Enum
+
+
+class TokenType(str, Enum):
+    ACCESS = "access"
+    REFRESH = "refresh"
+
+
+class LoginOut(BaseModel):
+    access: str = Field(description="Token de acesso")
+    refresh: str = Field(description="Token de atualização")
+
+
+class TokenRefreshIn(BaseModel):
+    """Dados que são submetidos para obter um novo token de acesso"""
+
+    refresh: str = Field(description="Token de atualização")
+
+
+class TokenRefreshOut(BaseModel):
+    """Dados que são retornados após solicitação de refresh token"""
+
+    access: str = Field(description="Novo token de acesso")
+
+
+class TokenStorage(BaseModel):
+    """Dados que são armazenados dentro do token JWT"""
+
+    user_id: int = Field(description="Identificador do usuário")
+    sub: str = Field(description="Nome do usuário")
+    token_type: TokenType = Field(description="Tipo do token")
+    exp: timedelta = Field(description="Tempo de expiração do token")
