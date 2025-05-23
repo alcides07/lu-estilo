5e484e5b08d9da1e1c3b1054a9891d30074e484f
alcides07 <alcidesdantasdj@gmail.com>
2025-05-23 16:34:42 -0300

    feat: implementa rota de leitura de clientes. (#25)

--- api/routers/client.py
+++ api/routers/client.py
@@ -1,14 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from api.dependencies.get_user_authenticated import get_user_authenticated
 from api.dependencies.get_session_db import SessionDep
-from api.orm.client import create_client
+from api.filters.client import ClientFilter
+from api.orm.client import create_client, list_clients
 from api.schemas.client import ClientCreate, ClientRead
+from api.schemas.utils.pagination import PaginationSchema
+from api.schemas.utils.responses import ResponsePagination
 
 
 router = APIRouter(
     prefix="/clients",
     tags=["clients"],
-    # dependencies=[Depends(get_authenticated_user)],
+    dependencies=[Depends(get_user_authenticated)],
 )
+
+
+@router.get("/", status_code=201)
+async def list(
+    session: SessionDep,
+    pagination: PaginationSchema = Depends(),
+    filters: ClientFilter = Depends(),
+) -> ResponsePagination[ClientRead]:
+
+    data = list_clients(session=session, pagination=pagination, filters=filters)
+    return ResponsePagination(data=data)
 
 
 @router.post("/", response_model=ClientRead, status_code=201)
