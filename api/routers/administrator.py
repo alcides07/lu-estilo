2a83ed5e465839559d2a889d97e52b462ad25961
alcides07 <alcidesdantasdj@gmail.com>
2025-05-23 21:26:35 -0300

    refactor: renomeia funcao de checar dono de user. (#330

--- api/routers/administrator.py
+++ api/routers/administrator.py
@@ -4,7 +4,7 @@ from api.filters.administrator import AdministratorFilter
 from api.models.user import User
 from api.orm.administrator import create_administrator, list_administrators
 from api.permissions.administrator import is_administrator
-from api.permissions.utils.check_owner_permission import check_owner_permission
+from api.permissions.utils.check_owner_permission import check_ower_user_permission
 from api.schemas.administrator import AdministratorCreate, AdministratorRead
 from api.dependencies.get_user_authenticated import get_user_authenticated
 from api.schemas.utils.pagination import PaginationSchema
@@ -40,7 +40,7 @@ async def create(
     current_user: User = Depends(get_user_authenticated),
 ):
 
-    await check_owner_permission(administrator.user_id, current_user)
+    await check_ower_user_permission(administrator.user_id, current_user)
 
     data = create_administrator(administrator, session)
     return data
