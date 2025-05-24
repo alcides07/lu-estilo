9b463ac8cbd3c82026beb4199e7547b6482f5945
alcides07 <alcidesdantasdj@gmail.com>
2025-05-24 13:10:24 -0300

    feat: implementa rotas de crud de product. (#36)

---
+++ api/routers/product.py
@@ -0,0 +1,81 @@
+from fastapi import APIRouter, Depends, Path, Response, status
+from api.dependencies.get_user_authenticated import get_user_authenticated
+from api.dependencies.get_session_db import SessionDep
+from api.filters.product import ProductFilter
+from api.permissions.administrator import is_administrator
+from api.schemas.product import ProductCreate, ProductRead, ProductUpdate
+from api.schemas.utils.pagination import PaginationSchema
+from api.schemas.utils.responses import ResponsePagination, ResponseUnit
+from api.services.product import ProductService
+
+
+router = APIRouter(
+    prefix="/products",
+    tags=["products"],
+)
+
+
+@router.get("/")
+async def list(
+    session: SessionDep,
+    pagination: PaginationSchema = Depends(),
+    filters: ProductFilter = Depends(),
+) -> ResponsePagination[ProductRead]:
+
+    service = ProductService(session)
+    data = service.list_products(pagination=pagination, filters=filters)
+    return ResponsePagination(data=data)
+
+
+@router.get(
+    "/{id}/",
+)
+async def read(
+    session: SessionDep,
+    id: int = Path(description="Identificador do produto"),
+) -> ResponseUnit[ProductRead]:
+
+    service = ProductService(session)
+    data = service.read_product(id)
+    return ResponseUnit(data=data)
+
+
+@router.post(
+    "/",
+    status_code=201,
+    dependencies=[Depends(is_administrator), Depends(get_user_authenticated)],
+)
+async def create(
+    product: ProductCreate,
+    session: SessionDep,
+) -> ProductRead:
+    service = ProductService(session)
+    return service.create_product(product=product)
+
+
+@router.put(
+    "/{id}/",
+    dependencies=[Depends(is_administrator), Depends(get_user_authenticated)],
+)
+async def update(
+    product: ProductUpdate,
+    session: SessionDep,
+    id: int = Path(description="Identificador do produto"),
+) -> ProductRead:
+    service = ProductService(session)
+    data = service.update_product(id, product)
+    return data
+
+
+@router.delete(
+    "/{id}/",
+    status_code=204,
+    dependencies=[Depends(is_administrator), Depends(get_user_authenticated)],
+)
+async def delete(
+    session: SessionDep,
+    id: int = Path(description="Identificador do produto"),
+):
+    service = ProductService(session)
+    await service.delete_product(id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
