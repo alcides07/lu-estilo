5764a6898efe2aa413d9bd1516e6a13c630c1f44
alcides07 <alcidesdantasdj@gmail.com>
2025-05-25 02:41:29 -0300

    feat: implementa tabela intermediaria product e order. (#38)

---
+++ api/models/order_product.py
@@ -0,0 +1,21 @@
+from sqlalchemy import ForeignKey
+from database.config import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from decimal import Decimal
+from sqlalchemy.types import DECIMAL
+
+
+class OrderProduct(Base):
+    __tablename__ = "order_product"
+
+    order_id: Mapped[int] = mapped_column(
+        ForeignKey("order.id", ondelete="RESTRICT"),
+        primary_key=True,
+    )
+    product_id: Mapped[int] = mapped_column(
+        ForeignKey("product.id", ondelete="RESTRICT"), primary_key=True
+    )
+    quantity: Mapped[int]
+    unit_price: Mapped[Decimal] = mapped_column(DECIMAL(10, 2))
+    order: Mapped["Order"] = relationship("Order", back_populates="products")
+    product: Mapped["Product"] = relationship("Product", back_populates="orders")
