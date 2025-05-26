c725d58fffb5a3eefcb73a6046308f50af6ba465
alcides07 <alcidesdantasdj@gmail.com>
2025-05-26 11:28:11 -0300

    fix: chama sentry sob demanda. (#43)

--- api/main.py
+++ api/main.py
@@ -5,15 +5,16 @@ from routers import all_routers
 from database.config import Base, engine
 from decouple import config
 
-SENTRY_DSN = config("SENTRY_DSN")
+SENTRY_DSN = config("SENTRY_DSN", default=None)
 
-sentry_sdk.init(
-    dsn=SENTRY_DSN,
-    send_default_pii=True,
-    traces_sample_rate=1.0,
-    profile_session_sample_rate=1.0,
-    profile_lifecycle="trace",
-)
+if SENTRY_DSN is not None:
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        send_default_pii=True,
+        traces_sample_rate=1.0,
+        profile_session_sample_rate=1.0,
+        profile_lifecycle="trace",
+    )
 
 
 @asynccontextmanager
