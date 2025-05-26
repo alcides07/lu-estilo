7d3514b13421b9fba967adc51eb80fed4855a971
alcides07 <alcidesdantasdj@gmail.com>
2025-05-26 15:27:58 -0300

    docs: inclui exemplo em schema de cliente. (#43)

--- api/schemas/client.py
+++ api/schemas/client.py
@@ -1,19 +1,39 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, Field
 from schemas.user import UserRead
 
 
 class ClientRead(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-
     id: int = Field(description="Identificador do cliente")
     user: UserRead = Field(description="Usuário referente ao cliente")
     cpf: str = Field(max_length=11, description="CPF do cliente")
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "id": 1,
+                "user": {
+                    "created_at": "2025-05-26T11:47:47.573Z",
+                    "email": "maria@email.com",
+                    "id": 1,
+                    "name": "maria",
+                    "updated_at": "2025-05-26T11:47:47.573Z",
+                },
+                "cpf": "11122233344",
+            }
+        }
+    }
 
 
 class ClientCreate(BaseModel):
     user_id: int = Field(description="Identificador do usuário")
     cpf: str = Field(max_length=11, description="CPF do cliente")
 
+    model_config = {
+        "json_schema_extra": {"example": {"user_id": 1, "cpf": "11122233344"}}
+    }
+
 
 class ClientUpdate(BaseModel):
     cpf: str = Field(max_length=11, description="CPF do cliente")
+
+    model_config = {"json_schema_extra": {"example": {"cpf": "11122233344"}}}
