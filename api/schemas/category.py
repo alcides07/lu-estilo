6caa67176eaa2e9a9266b9a17a32c575a87661f7
alcides07 <alcidesdantasdj@gmail.com>
2025-05-26 11:14:10 -0300

    docs: inclui exemplos de categorias. (#43)

--- api/schemas/category.py
+++ api/schemas/category.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, Field
 
 
 class CategoryBase(BaseModel):
@@ -10,10 +10,25 @@ class CategoryBase(BaseModel):
 
 
 class CategoryRead(CategoryBase):
-    model_config = ConfigDict(from_attributes=True)
-
     id: int = Field(description="Identificador da categoria")
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "id": 1,
+                "name": "inverno",
+                "description": "roupas de inverno",
+            }
+        }
+    }
 
 
 class CategoryCreate(CategoryBase):
-    pass
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "name": "inverno",
+                "description": "roupas de inverno",
+            }
+        }
+    }
