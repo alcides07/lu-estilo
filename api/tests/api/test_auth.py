17e484257e61a5c7e0228014d583ca4f35547069
alcides07 <alcidesdantasdj@gmail.com>
2025-05-26 11:27:54 -0300

    test: fix test refresh token. (#43)

--- api/tests/api/test_auth.py
+++ api/tests/api/test_auth.py
@@ -2,7 +2,7 @@ def test_refresh_token(client):
     refresh_token = client["refresh_token"]
 
     response = client["agent"].post(
-        "/auth/refresh-token/", params={"refresh": refresh_token}
+        "/auth/refresh-token/", json={"refresh": refresh_token}
     )
     assert response.status_code == 200
 
