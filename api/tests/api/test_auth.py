b0a7aa5ef9c677065421f66cbc6306fb0be8a491
alcides07 <alcidesdantasdj@gmail.com>
2025-05-26 01:40:41 -0300

    test: implementa teste de refresh token. (#42)

---
+++ api/tests/api/test_auth.py
@@ -0,0 +1,10 @@
+def test_refresh_token(client):
+    refresh_token = client["refresh_token"]
+
+    response = client["agent"].post(
+        "/auth/refresh-token/", params={"refresh": refresh_token}
+    )
+    assert response.status_code == 200
+
+    response_data = response.json()
+    assert response_data["access"] is not None
