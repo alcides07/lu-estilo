f18e4eef2bfbec0c55f5da4af19fdbd954595f02
alcides07 <alcidesdantasdj@gmail.com>
2025-05-26 15:26:12 -0300

    docs: documenta filtro de clientes. (#43)

--- api/filters/client.py
+++ api/filters/client.py
@@ -5,12 +5,38 @@ class ClientFilter:
     def __init__(
         self,
         user__name: str = Query(
-            default=None, max_length=255, description="Nome do usuário"
+            default=None,
+            max_length=255,
+            description="""
+    Filtra por nome do usuário.
+    ### Características:
+        - Busca por correspondência parcial (não distingue minúsculas e maiúsculas)
+        - Máximo de 255 caracteres
+        - Exemplo: `?user__name=ca` encontra "carol" e "carla"
+    """,
         ),
         user__email: str = Query(
-            default=None, max_length=255, description="E-mail do usuário"
+            default=None,
+            max_length=255,
+            description="""
+    Filtra por e-mail do usuário.
+    ### Características:
+        - Busca por correspondência parcial (não distingue minúsculas e maiúsculas)
+        - Máximo de 255 caracteres
+        - Exemplo: `?user__email=car` encontra "carol@email.com" e "carla@email.com"
+    """,
         ),
-        cpf: str = Query(default=None, max_length=11, description="CPF do cliente"),
+        cpf: str = Query(
+            default=None,
+            max_length=11,
+            description="""
+    Filtra por CPF do usuário.
+    ### Características:
+        - Busca por correspondência parcial
+        - Máximo de 11 caracteres
+        - Exemplo: `?cpf=555` encontra "555.444.333-22" e "333.444.555-66"
+    """,
+        ),
     ):
         self.cpf = cpf
         self.user__name = user__name
