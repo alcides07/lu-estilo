e3c40291c2446dd54b86673670fdead5ea8bdd30
alcides07 <alcidesdantasdj@gmail.com>
2025-05-26 11:14:24 -0300

    docs: inclui exemplos de users. (#43)

--- api/schemas/user.py
+++ api/schemas/user.py
@@ -1,10 +1,8 @@
-from pydantic import BaseModel, ConfigDict, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field
 from datetime import datetime
 
 
 class UserRead(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-
     id: int = Field(description="Identificador do usuário")
     name: str = Field(max_length=255, description="Nome do usuário")
     email: EmailStr = Field(max_length=255, description="E-mail do usuário")
@@ -13,8 +11,30 @@ class UserRead(BaseModel):
         description="Data em que algum dado do usuário foi editado pela última vez"
     )
 
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "id": 1,
+                "name": "maria",
+                "email": "maria@email.com",
+                "created_at": "2025-05-26T11:47:47.573Z",
+                "updated_at": "2025-05-26T11:47:47.573Z",
+            }
+        }
+    }
+
 
 class UserCreate(BaseModel):
     name: str = Field(max_length=255, description="Nome do usuário")
     password: str = Field(max_length=255, description="Senha do usuário")
     email: EmailStr = Field(max_length=255, description="E-mail do usuário")
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "name": "maria",
+                "password": "123456",
+                "email": "maria@email.com",
+            }
+        }
+    }
