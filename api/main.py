89c750e5874b562a650c37228ed44861b9e28a11
alcides07 <alcidesdantasdj@gmail.com>
2025-05-26 04:06:55 -0300

    docs: insere descricao geral da API. (#43)

--- api/main.py
+++ api/main.py
@@ -11,7 +11,21 @@ async def lifespan(app: FastAPI):
     engine.dispose()
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    lifespan=lifespan,
+    title="API Lu Estilo - Sistema Comercial",
+    docs_url=None,
+    redoc_url=None,
+    description="""## 📌 Visão Geral    
+    A API Lu Estilo é a solução tecnológica desenvolvida para potencializar as operações comerciais 
+    da Lu Estilo, empresa de confecção que busca expandir seus canais de vendas.
+    ### 🚀 Solução Proposta
+    Esta API RESTful fornece:
+    - Gestão de clientes
+    - Controle de estoque de produtos
+    - Gerenciamento de pedidos
+    """,
+)
 
 
 for router in all_routers:
